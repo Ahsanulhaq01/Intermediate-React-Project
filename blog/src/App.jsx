8fb07519e620be2a2e7fb5c707aa7cd41c994623
Ahsanulhaq01 <ulhaqahsan276@gmail.com>
2025-08-25 12:57:25 +0500

    create HTML for body content

--- blog/src/App.jsx
+++ blog/src/App.jsx
@@ -6,6 +6,28 @@ function App() {
   return (
     <>
     <Navbar/>
+    <div className="homepage-container">
+      <div className="blog-container">
+        <div className="blog-title">
+          <p className="title-text truncate"><b>sunt aut facere repellat provident occaecati excepturi optio reprehenderit</b></p>
+        </div>
+        <div className="blog-body">
+          <p className="body-content truncate">
+            quia et suscipit suscipit recusandae consequuntur expedita et cum reprehenderit molestiae ut ut quas totam nostrum rerum est autem sunt rem eveniet architecto
+          </p>
+        </div>
+      </div>
+      <div className="blog-container">
+        <div className="blog-title">
+          <p className="title-text truncate"><b>sunt aut facere repellat provident occaecati excepturi optio reprehenderit</b></p>
+        </div>
+        <div className="blog-body">
+          <p className="body-content truncate">
+            quia et suscipit suscipit recusandae consequuntur expedita et cum reprehenderit molestiae ut ut quas totam nostrum rerum est autem sunt rem eveniet architecto
+          </p>
+        </div>
+      </div>
+    </div>
     </>
   )
 }
