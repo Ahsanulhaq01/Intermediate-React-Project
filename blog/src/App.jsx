f6e2994266f829f1b2cd62635229fc4aed9b1338
Ahsanulhaq01 <ulhaqahsan276@gmail.com>
2025-08-25 12:37:07 +0500

    Set component for navbar

--- blog/src/App.jsx
+++ blog/src/App.jsx
@@ -1,21 +1,11 @@
 import './App.css'
+import Navbar from './Navabar/Navbar'
 function App() {
  
 
   return (
     <>
-    <div className="nav-container">
-        <div className="logo">
-          <img src="./menu-logo.png" alt="menu-logo" className="menu-img" />
-        </div>
-        <div className="nav-right-section">
-        <ul className="nav-ul">
-          <li key={1}>Home</li>
-          <li key={2}>All Post</li>
-          <li key={3}>Contact</li>
-        </ul>
-        </div>
-    </div>
+    <Navbar/>
     </>
   )
 }
---
+++ blog/src/Navabar/Navbar.jsx
@@ -0,0 +1,21 @@
+import './navbar.css'
+function Navbar(){
+    return(
+        <>
+         <div className="nav-container">
+        <div className="logo">
+          <img src="./menu-logo.png" alt="menu-logo" className="menu-img" />
+        </div>
+        <div className="nav-right-section">
+        <ul className="nav-ul">
+          <li key={1}>Home</li>
+          <li key={2}>All Post</li>
+          <li key={3}>Contact</li>
+        </ul>
+        </div>
+    </div>
+        </>
+    )
+}
+
+export default Navbar;
