4979237fe0092a380146b062af151e2f7722bf02
Ahsanulhaq01 <ulhaqahsan276@gmail.com>
2025-08-26 01:37:50 +0500

    add contact page

--- blog/src/App.jsx
+++ blog/src/App.jsx
@@ -2,6 +2,7 @@ import Navbar from './Navabar/Navbar'
 import AllPost from './pages/AllPost'
 import { Routes,Route } from 'react-router'
 import Homepage from './pages/Homepage'
+import ContactPage from './pages/ContactPage'
 function App() {
   return(
 
@@ -9,6 +10,7 @@ function App() {
     <Routes>
       <Route index element ={<Homepage/>}></Route>
       <Route path='post' element={<AllPost/>}></Route>
+      <Route path='contact' element={<ContactPage/>}></Route>
       
     </Routes>
     
---
+++ blog/src/pages/ContactPage.jsx
@@ -0,0 +1,17 @@
+import Navbar from '../Navabar/Navbar';
+import './contactpage.css'
+function ContactPage(){
+    return(
+        <>
+        <Navbar/>
+        <div className="contact-container">
+            <p className="name">Name : Ahsanulhaq</p>
+            <p className="email">Email : ulhaqahsan276@gmail.com</p>
+            <p className="mobileno">Mobile No : 0300-1234567</p>
+            <p className="cnic">CNIC : 12345-123232-9</p>
+        </div>
+        </>
+    )
+}
+
+export default ContactPage;
