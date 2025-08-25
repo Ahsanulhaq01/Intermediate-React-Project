0860dd3e1d56830e6e24b2555eb88b9bc0b3485e
Ahsanulhaq01 <ulhaqahsan276@gmail.com>
2025-08-25 12:20:11 +0500

    added  navbar HTML

--- blog/src/App.jsx
+++ blog/src/App.jsx
@@ -3,7 +3,22 @@ function App() {
  
 
   return (
-    <></>
+    <>
+    <div className="nav-container">
+      <nav>
+        <div className="logo">
+          <img src="./menu-logo.png" alt="menu-logo" className="menu-img" />
+        </div>
+        <div className="nav-right-section">
+        <ul className="nav-ul">
+          <li key={1}>Home</li>
+          <li key={2}>All Post</li>
+          <li key={3}>Contact</li>
+        </ul>
+        </div>
+      </nav>
+    </div>
+    </>
   )
 }
 
