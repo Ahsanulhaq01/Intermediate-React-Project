85c23da390183f95f283febeb0e07f79240cfdad
Ahsanulhaq01 <ulhaqahsan276@gmail.com>
2025-09-13 15:07:32 +0500

    create Header Component

---
+++ ecommerce-fronted/src/components/Header.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import "./header.css";
+function Header() {
+  return (
+    <>
+      <div className="header">
+        <div className="header-name">
+            <p>Ecommerce Store</p>
+        </div>
+        <div className="search-bar">
+          <div className="search-bar-input">
+            <input type="text" placeholder="search" />
+          </div>
+
+          <div className="search-bar-arrow-icon">
+            <i class="fa-solid fa-arrow-right"></i>
+          </div>
+        
+        </div>
+        <div className="header-menu">
+            <div className="orders">
+                <p>Orders</p>
+            </div>
+            <div className="cart-icon">
+                <i class="fa-solid fa-cart-shopping"></i>
+                <div className="cart-quantity">
+                    <p>3</p>
+                </div>
+            </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Header;
---
+++ ecommerce-fronted/src/pages/HomePage.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Header from "../components/Header";
+import ".././App.css";
+function HomePage() {
+  return (
+    <>
+    <Header/>
+    
+    </>
+  );
+}
+
+export default HomePage;
