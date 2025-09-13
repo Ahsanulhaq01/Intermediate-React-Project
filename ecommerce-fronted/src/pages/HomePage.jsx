879cd9ceacef9076f4017a457e5e31bad56a7bf1
Ahsanulhaq01 <ulhaqahsan276@gmail.com>
2025-09-13 17:15:52 +0500

    create Homepage and import image for it

--- ecommerce-fronted/src/pages/HomePage.jsx
+++ ecommerce-fronted/src/pages/HomePage.jsx
@@ -5,7 +5,243 @@ function HomePage() {
   return (
     <>
     <Header/>
-    
+    <div className="products-grid">
+        <div className="products">
+            <div className="product-image">
+                <img src="images/products/athletic-cotton-socks-6-pairs.jpg" alt="" width={250} />
+            </div>
+            <div className="product-name">
+                <p className="name-of-product">
+                    Black and Gray Athletic Cotton Socks - 6 Pairs"
+                </p>
+            </div>
+            <div className="product-rating">
+                <img src="images/ratings/rating-40.png" alt="" />
+            </div>
+            <div className="product-price">
+                <p>$10.49</p>
+            </div>
+            <div className="select-quantity">
+                <select name="quantity-of-product" id="">
+                    <option value="1">1</option>
+                    <option value="2">2</option>
+                    <option value="3">3</option>
+                    <option value="4">4</option>
+                    <option value="5">5</option>
+                    <option value="6">6</option>
+                    <option value="7">7</option>
+                    <option value="8">8</option>
+                    <option value="9">9</option>
+                    <option value="10">10</option>
+
+                </select>
+            </div>
+            <div className="product-spacer"></div>
+            <div className="added-to-cart" style={{opacity : 0}}>
+                Added
+                <img src="" alt="" />
+            </div>
+            <div className="add-to-cart-buttons">
+                <button className="add-to-cart-btn">add to cart</button>
+            </div>
+        </div>
+        <div className="products">
+            <div className="product-image">
+                <img src="images/products/intermediate-composite-basketball.jpg" alt="" width={250} />
+            </div>
+            <div className="product-name">
+                <p className="name-of-product">
+                    Intermediate Size Basketball
+                </p>
+            </div>
+            <div className="product-rating">
+                <img src="images/ratings/rating-5.png" alt="" />
+            </div>
+            <div className="product-price">
+                <p>$20.95</p>
+            </div>
+            <div className="select-quantity">
+                <select name="quantity-of-product" id="">
+                    <option value="1">1</option>
+                    <option value="2">2</option>
+                    <option value="3">3</option>
+                    <option value="4">4</option>
+                    <option value="5">5</option>
+                    <option value="6">6</option>
+                    <option value="7">7</option>
+                    <option value="8">8</option>
+                    <option value="9">9</option>
+                    <option value="10">10</option>
+
+                </select>
+            </div>
+            <div className="product-spacer"></div>
+            <div className="added-to-cart" style={{opacity : 0}}>
+                Added
+                <img src="" alt="" />
+            </div>
+            <div className="add-to-cart-buttons">
+                <button className="add-to-cart-btn">add to cart</button>
+            </div>
+        </div>
+        <div className="products">
+            <div className="product-image">
+                <img src="images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg" alt="" width={250} />
+            </div>
+            <div className="product-name">
+                <p className="name-of-product">
+                    Adults Plain Cotton T-Shirt - 2 Pack
+                </p>
+            </div>
+            <div className="product-rating">
+                <img src="images/ratings/rating-10.png" alt="" />
+            </div>
+            <div className="product-price">
+                <p>$7.99</p>
+            </div>
+            <div className="select-quantity">
+                <select name="quantity-of-product" id="">
+                    <option value="1">1</option>
+                    <option value="2">2</option>
+                    <option value="3">3</option>
+                    <option value="4">4</option>
+                    <option value="5">5</option>
+                    <option value="6">6</option>
+                    <option value="7">7</option>
+                    <option value="8">8</option>
+                    <option value="9">9</option>
+                    <option value="10">10</option>
+
+                </select>
+            </div>
+            <div className="product-spacer"></div>
+            <div className="added-to-cart" style={{opacity : 0}}>
+                Added
+                <img src="" alt="" />
+            </div>
+            <div className="add-to-cart-buttons">
+                <button className="add-to-cart-btn">add to cart</button>
+            </div>
+        </div>
+        <div className="products">
+            <div className="product-image">
+                <img src="images/products/2-slot-toaster-white.jpg" alt="" />
+            </div>
+            <div className="product-name">
+                <p className="name-of-product">
+                    2 Slot Toaster - White
+                </p>
+            </div>
+            <div className="product-rating">
+                <img src="images/ratings/rating-45.png" alt="" />
+            </div>
+            <div className="product-price">
+                <p>$18.99</p>
+            </div>
+            <div className="select-quantity">
+                <select name="quantity-of-product" id="">
+                    <option value="1">1</option>
+                    <option value="2">2</option>
+                    <option value="3">3</option>
+                    <option value="4">4</option>
+                    <option value="5">5</option>
+                    <option value="6">6</option>
+                    <option value="7">7</option>
+                    <option value="8">8</option>
+                    <option value="9">9</option>
+                    <option value="10">10</option>
+
+                </select>
+            </div>
+            <div className="product-spacer"></div>
+            <div className="added-to-cart" style={{opacity : 0}}>
+                Added
+                <img src="" alt="" />
+            </div>
+            <div className="add-to-cart-buttons">
+                <button className="add-to-cart-btn">add to cart</button>
+            </div>
+        </div>
+        <div className="products">
+            <div className="product-image">
+                <img src="images/products/elegant-white-dinner-plate-set.jpg" alt=""/>
+            </div>
+            <div className="product-name">
+                <p className="name-of-product">
+                    2 Piece White Dinner Plate Set
+                </p>
+            </div>
+            <div className="product-rating">
+                <img src="images/ratings/rating-50.png" alt="" />
+            </div>
+            <div className="product-price">
+                <p>$20.67</p>
+            </div>
+            <div className="select-quantity">
+                <select name="quantity-of-product" id="">
+                    <option value="1">1</option>
+                    <option value="2">2</option>
+                    <option value="3">3</option>
+                    <option value="4">4</option>
+                    <option value="5">5</option>
+                    <option value="6">6</option>
+                    <option value="7">7</option>
+                    <option value="8">8</option>
+                    <option value="9">9</option>
+                    <option value="10">10</option>
+
+                </select>
+            </div>
+            <div className="product-spacer"></div>
+            <div className="added-to-cart" style={{opacity : 0}}>
+                Added
+                <img src="" alt="" />
+            </div>
+            <div className="add-to-cart-buttons">
+                <button className="add-to-cart-btn">add to cart</button>
+            </div>
+        </div>
+        <div className="products">
+            <div className="product-image">
+                <img src="images/products/3-piece-cooking-set.jpg" alt="" />
+            </div>
+            <div className="product-name">
+                <p className="name-of-product">
+                   3 Piece Non-Stick, Black Cooking Pot Set
+                </p>
+            </div>
+            <div className="product-rating">
+                <img src="images/ratings/rating-10.png" alt="" />
+            </div>
+            <div className="product-price">
+                <p>$10.49</p>
+            </div>
+            <div className="select-quantity">
+                <select name="quantity-of-product" id="">
+                    <option value="1">1</option>
+                    <option value="2">2</option>
+                    <option value="3">3</option>
+                    <option value="4">4</option>
+                    <option value="5">5</option>
+                    <option value="6">6</option>
+                    <option value="7">7</option>
+                    <option value="8">8</option>
+                    <option value="9">9</option>
+                    <option value="10">10</option>
+
+                </select>
+            </div>
+            <div className="product-spacer"></div>
+            <div className="added-to-cart" style={{opacity : 0}}>
+                Added
+                <img src="" alt="" />
+            </div>
+            <div className="add-to-cart-buttons">
+                <button className="add-to-cart-btn">add to cart</button>
+            </div>
+        </div>
+        
+    </div>
     </>
   );
 }
