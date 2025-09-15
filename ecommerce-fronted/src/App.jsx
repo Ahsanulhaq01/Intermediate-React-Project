8a9458b87ad6471cf37ed909068e6317ce39c404
Ahsanulhaq01 <ulhaqahsan276@gmail.com>
2025-09-16 00:08:56 +0500

    create new Route for Checkout Component

--- ecommerce-fronted/src/App.jsx
+++ ecommerce-fronted/src/App.jsx
@@ -1,12 +1,14 @@
 import HomePage from "./pages/HomePage";
 import { Route,Routes } from "react-router";
 import Orders from "./pages/Orders";
+import Checkout from "./pages/Checkout";
 
 function App(){
     return(
         <Routes>
             <Route index element={<HomePage/>}></Route>
             <Route path="orders" element={<Orders/>}></Route>
+            <Route path="checkout" element={<Checkout/>}></Route>
         </Routes>
     )
 }
