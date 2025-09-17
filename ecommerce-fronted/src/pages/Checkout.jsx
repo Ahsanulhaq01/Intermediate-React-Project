2a76d6e679834b24a2acf3f1d00fc4ade4fc9313
Ahsanulhaq01 <ulhaqahsan276@gmail.com>
2025-09-18 00:58:05 +0500

    make Order Component Responsive

--- ecommerce-fronted/src/pages/Checkout.jsx
+++ ecommerce-fronted/src/pages/Checkout.jsx
@@ -24,74 +24,8 @@ function Checkout() {
             <p className="reveiw-order">Review yours orders</p>
           </div>
           <div className="delivery-and-payment-container">
-            <div className="delivery-container">
-              <div className="delivery-data">
-                <p className="delivery-header">
-                  Delivery date:Monday, September 22
-                </p>
-              </div>
-              <div className="delivery-option-and-product-details">
-                <div className="product-details-container">
-                  <img
-                    src="images/products/athletic-cotton-socks-6-pairs.jpg"
-                    alt="image"
-                   
-                  />
-                  <div className="products-details">
-                    <p className="name-of-products">
-                      Adults Plain Cotton T-Shirt - 2 Pack
-                    </p>
-                    <p className="product-price">$7.99</p>
-                    <div className="quantity-modification-container">
-                      <div className="product-quantity">Quantity : 1</div>
-                      <div className="quantity-btns">
-                        <button>Update</button>
-                        <button>Delete</button>
-                      </div>
-                    </div>
-                  </div>
-                <div className="select-delivery-option-container">
-                    <div className="option-heading">
-                        <p>Choose a delivery option:</p>
-                    </div>
-                    <div className="free-shipping-container">
-                        <input type="radio" name="option" />
-                        <div className="date-delivery-method">
-                            <p className="date-para">
-                                Monday, September 22
-                            </p>
-                            <p className="shipping-method-para">
-                                Free Shipping
-                            </p>
-                        </div>
-                    </div>
-                     <div className="free-shipping-container">
-                        <input type="radio" name="option" />
-                        <div className="date-delivery-method">
-                            <p className="date-para">
-                                Thursday, September 18
-                            </p>
-                            <p className="shipping-method-para">
-                                $4.99-Shipping
-                            </p>
-                        </div>
-                    </div>
-                     <div className="free-shipping-container">
-                        <input type="radio" name="option" />
-                        <div className="date-delivery-method">
-                            <p className="date-para">
-                               Tuesday, September 16
-                            </p>
-                            <p className="shipping-method-para">
-                                $9.99-Shipping
-                            </p>
-                        </div>
-                    </div>
-                </div>
-                </div>
-              </div>
-            </div>
-            <div className="payment-summary-container">
+             <aside>
+               <div className="payment-summary-container">
               <div className="payment-summary">
                 <p className="payment-summary-heading">Payment Summary</p>
                 <div className="items-container">
@@ -121,6 +55,82 @@ function Checkout() {
                 </div>
               </div>
             </div>
+            </aside>
+            <div className="delivery-container">
+              
+              <div className="delivery-option-and-product-details">
+                <div className="delivery-data">
+                <p className="delivery-header">
+                  Delivery date:Monday, September 22
+                </p>
+              </div>
+                <div className="product-details-container">
+                  <img
+                    src="images/products/athletic-cotton-socks-6-pairs.jpg"
+                    alt="image"
+                   
+                  />
+                  <div className="products-details">
+                    <p className="name-of-products">
+                      Adults Plain Cotton T-Shirt - 2 Pack
+                    </p>
+                    <p className="product-price">$7.99</p>
+                    <div className="quantity-modification-container">
+                      <div className="product-quantity">Quantity : 1</div>
+                      <div className="quantity-btns">
+                        <button>Update</button>
+                        <button>Delete</button>
+                      </div>
+                    </div>
+                  </div>
+                
+                </div>
+              </div>
+              <div className="select-delivery-option-container">
+                    <div className="option-heading">
+                        <p>Choose a delivery option:</p>
+                    </div>
+                    <div className="free-shipping-container">
+                        <input type="radio" 
+                        value={1}
+                        name="option" />
+                        <div className="date-delivery-method">
+                            <p className="date-para">
+                                Monday, September 22
+                            </p>
+                            <p className="shipping-method-para">
+                                Free Shipping
+                            </p>
+                        </div>
+                    </div>
+                     <div className="free-shipping-container">
+                        <input type="radio" 
+                        value={1}
+                        name="option" />
+                        <div className="date-delivery-method">
+                            <p className="date-para">
+                                Thursday, September 18
+                            </p>
+                            <p className="shipping-method-para">
+                                $4.99-Shipping
+                            </p>
+                        </div>
+                    </div>
+                     <div className="free-shipping-container">
+                        <input type="radio" 
+                        value={1}
+                        name="option" />
+                        <div className="date-delivery-method">
+                            <p className="date-para">
+                               Tuesday, September 16
+                            </p>
+                            <p className="shipping-method-para">
+                                $9.99-Shipping
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>           
           </div>
         </div>
       </div>
