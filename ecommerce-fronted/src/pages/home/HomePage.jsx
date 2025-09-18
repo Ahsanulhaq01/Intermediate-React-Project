627f5f9000178c55c5b6e57414e785bf0fe83d9d
Ahsanulhaq01 <ulhaqahsan276@gmail.com>
2025-09-18 22:25:57 +0500

    create separate component from Homepage Component

---
+++ ecommerce-fronted/src/pages/home/HomePage.jsx
@@ -0,0 +1,17 @@
+import Header from "../../components/Header";
+import ProductGrid from "./ProductGrid";
+import './homepage.css'
+import "../.././App.css";
+function HomePage() {
+    
+
+    
+  return (
+    <>
+    <Header/>
+    <ProductGrid />
+    </>
+  );
+}
+
+export default HomePage;
---
+++ ecommerce-fronted/src/pages/home/ProductGrid.jsx
@@ -0,0 +1,26 @@
+import { useState ,useEffect } from 'react';
+import axios from 'axios'
+import Product from './Product';
+
+function ProductGrid() {
+    const [products , setProducts] = useState([]);
+    const getProductData = async ()=>{
+        const response = await axios.get('api/products/');
+        setProducts(response.data)
+    }
+    useEffect(()=>{
+        getProductData()
+    },[])
+    
+  return (
+    <div className="products-grid">
+        {products.map((product)=>{
+            return(
+            <Product product={product}/>
+            )
+        })}        
+    </div>
+  )
+}
+
+export default ProductGrid
