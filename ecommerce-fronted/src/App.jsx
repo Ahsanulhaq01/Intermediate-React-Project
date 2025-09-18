fb8a1967e9f2e506ac4503c92a87291424efdac6
Ahsanulhaq01 <ulhaqahsan276@gmail.com>
2025-09-18 17:10:49 +0500

    create HTML from data fo cart product in Checkout Component

--- ecommerce-fronted/src/App.jsx
+++ ecommerce-fronted/src/App.jsx
@@ -3,13 +3,21 @@ import { Route,Routes } from "react-router";
 import Orders from "./pages/Orders";
 import Checkout from "./pages/Checkout";
 import Tracking from "./pages/Tracking";
+import { useState} from "react";
+import axios from "axios";
 
 function App(){
+    const [cart , setCart] = useState([])
+  const loadCart = async()=>{
+    const response = await axios.get('/api/cart-items?expand=product');
+    setCart(response.data)
+  }
+
     return(
         <Routes>
             <Route index element={<HomePage/>}></Route>
             <Route path="orders" element={<Orders/>}></Route>
-            <Route path="checkout" element={<Checkout/>}></Route>
+            <Route path="checkout" element={<Checkout cart={cart} loadCart={loadCart} />}></Route>
             <Route path="tracking" element={<Tracking/>}></Route>
         </Routes>
     )
