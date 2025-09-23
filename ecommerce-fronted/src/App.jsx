9b304d13adb20d80768112820b292312930edeb3
Ahsanulhaq01 <ulhaqahsan276@gmail.com>
2025-09-23 17:10:34 +0500

    remove the extra code

--- ecommerce-fronted/src/App.jsx
+++ ecommerce-fronted/src/App.jsx
@@ -4,21 +4,13 @@ import Orders from "./pages/orders/Orders";
 import Checkout from './pages/checkout/Checkout'
 import Tracking from "./pages/tracking/Tracking";
 import { ContextHome } from "./pages/home/homeContext/ContextHome";
-import { CartContext } from "./pages/checkout/cartContext/loadcart";
-import { useContext } from "react";
-import ProviderComponent from "./pages/checkout/cartContext/CartContextProvider";
 
 function App(){
-  const {cart , loadCart} = useContext(CartContext);
-  //   const [cart , setCart] = useState([])
-  // const loadCart = async()=>{
-  //   const response = await axios.get('/api/cart-items?expand=product');
-  //   setCart(response.data)
-  // }
+
     return(
       <ContextHome>
         <Routes>
-            <Route index element={<HomePage loadCart={loadCart} cart={cart}/>}></Route>
+            <Route index element={<HomePage/>}></Route>
             <Route path="orders" element={<Orders/>}></Route>
             <Route path="checkout" element={<Checkout/>}></Route>
             <Route path="tracking" element={<Tracking/>}></Route>
