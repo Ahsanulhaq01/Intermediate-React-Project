2491a05f08490d648bcb7b4fa85df9639ed5a6d2
Ahsanulhaq01 <ulhaqahsan276@gmail.com>
2025-09-24 19:15:59 +0500

    wrap main.jsx with Provider component of props store

--- ecommerce-fronted/src/main.jsx
+++ ecommerce-fronted/src/main.jsx
@@ -1,17 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import { BrowserRouter } from 'react-router'
-import CartContextProvider from './pages/checkout/cartContext/CartContextProvider.jsx'
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { BrowserRouter } from "react-router";
+import CartContextProvider from "./pages/checkout/cartContext/CartContextProvider.jsx";
+import { store } from "./redux/store.js";
+import { Provider } from "react-redux";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+createRoot(document.getElementById("root")).render(
+    <Provider store={store} >
     <BrowserRouter>
-    <CartContextProvider>
-      <App />
+      <CartContextProvider>
+        <App />
       </CartContextProvider>
     </BrowserRouter>
-    
-  </StrictMode>,
-)
+    </Provider>
+);
