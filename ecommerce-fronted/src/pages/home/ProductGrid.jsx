e40533de7ceeb7b5a714b0168a3d2db078b7fa9c
Ahsanulhaq01 <ulhaqahsan276@gmail.com>
2025-09-25 15:47:08 +0500

    load products from productSlice using redux-toolkit

--- ecommerce-fronted/src/pages/home/ProductGrid.jsx
+++ ecommerce-fronted/src/pages/home/ProductGrid.jsx
@@ -1,10 +1,16 @@
-import { useContext} from 'react';
+import { useContext, useEffect} from 'react';
 import { HomeContext } from './homeContext/HomeContext';
 import Product from './Product';
 import { CartContext } from '../checkout/cartContext/loadcart';
+import { useDispatch, useSelector } from 'react-redux';
+import { getProductData } from '../../redux/slices/productSlice';
 
 function ProductGrid() {
-    const products = useContext(HomeContext)
+  const dispatch = useDispatch();
+  useEffect(()=>{
+    dispatch(getProductData())
+  },[])
+  const {products} = useSelector(state => state.products)
     const {loadCart} = useContext(CartContext)
   return (
     <div className="products-grid">
