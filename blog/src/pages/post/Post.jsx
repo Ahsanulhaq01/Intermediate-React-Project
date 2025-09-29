35644566768976e1bf25b4ba1cdca757690b4939
Ahsanulhaq01 <ulhaqahsan276@gmail.com>
2025-09-29 23:32:49 +0500

    get single blogpost from dummyjson with its id

--- blog/src/pages/post/Post.jsx
+++ blog/src/pages/post/Post.jsx
@@ -1,11 +1,26 @@
+import { useParams } from "react-router";
 import Navbar from "../../Navabar/Navbar";
 import "./post.css";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 function Post() {
+  const {id} = useParams();
+  const [post , setPost] = useState([]);
+  async function singlePost() {
+    const response = await axios.get(`https://jsonplaceholder.typicode.com/posts/${id}`)
+    setPost(response.data);
+    console.log(response.data)
+  }
+  useEffect(()=>{
+    singlePost();
+  } , [id])
+  
+
   return (
     <>
-      {/* <Navbar/> */}
-      <div className="container">
+     <Navbar />
+      {!post ? <h1>Loading....</h1> : <div className="container">
         <div className="post-container">
           <div className="title">
             <div className="post-title-heading">
@@ -13,8 +28,7 @@ function Post() {
             </div>
             <div className="post-title">
               <p className="title-text text">
-                sunt aut facere repellat provident occaecati excepturi optio
-                reprehenderit
+                {post.title}
               </p>
             </div>
           </div>
@@ -24,14 +38,36 @@ function Post() {
             </div>
             <div className="post-body">
               <p className="body-text text">
-                quia et suscipit\nsuscipit recusandae consequuntur expedita et
-                cum\nreprehenderit molestiae uzt ut quas totam\nnostrum rerum
-                est autem sunt rem eveniet architecto"
+                {post.body}
               </p>
             </div>
           </div>
         </div>
-      </div>
+      </div>}
+      {/* <div className="container">
+        <div className="post-container">
+          <div className="title">
+            <div className="post-title-heading">
+              <h3 className="title-heading">Title</h3>
+            </div>
+            <div className="post-title">
+              <p className="title-text text">
+                {post.title}
+              </p>
+            </div>
+          </div>
+          <div className="body">
+            <div className="post-body-heading">
+              <h3 className="body-heading">Body</h3>
+            </div>
+            <div className="post-body">
+              <p className="body-text text">
+                {post.body}
+              </p>
+            </div>
+          </div>
+        </div>
+      </div> */}
     </>
   );
 }
